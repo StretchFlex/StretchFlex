b6b930566941be564addb05e19eae31b37aef1bb
KeeleyRemSSH <keeley.whitmer@siu.edu>
2026-04-27 11:33:56 -0500

    krishna - hashed password

--- auth/Auth/Program.cs
+++ auth/Auth/Program.cs
@@ -1,4 +1,8 @@
+using BCrypt.Net;
 var builder = WebApplication.CreateBuilder(args);
+
+var hash = BCrypt.Net.BCrypt.HashPassword("password");
+Console.WriteLine(hash);
 
 builder.Services.AddControllers();
 
